f7f97df4c064f398de2336413358c212847d807e
Lian Lee <zhuojuelee@mac.home>
2025-06-23 22:13:55 -0400

    feat: init vite

---
+++ README.md
@@ -0,0 +1,12 @@
+# PFC710 - Assignment 1
+
+### Setup
+Prerequisitites:
+- AWS account
+  - AWS credentials
+- NodeJS v22
+- Python and `pip`
+
+
+Running the frontend:
+- `yarn install`
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+
+const filesNeedToExclude = ['scraper/**'];
+
+const filesPathToExclude = filesNeedToExclude.map((src) => {
+  return fileURLToPath(new URL(src, import.meta.url));
+});
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        ...filesPathToExclude
+      ]
+    }
+  }
+})
