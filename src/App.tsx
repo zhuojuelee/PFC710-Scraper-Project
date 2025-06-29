886254e8cf37340c4a686c25fba8baad45a7ceb9
Lian Lee <zhuojuelee@mac.home>
2025-06-28 23:05:35 -0400

    fix: change icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, Divider, Typography } from '@mui/material';
 import DataTable from './components/DataTable';
 import useTableData from './hooks/useTableData';
 import ActionButtons from './components/ActionButtons';
-import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider, useAtom } from 'jotai';
 import shoesAtom from './atoms/shoesAtom';
@@ -19,7 +19,7 @@ function App() {
         <Box sx={{ p:2 }}>
           <Box sx={{ display: 'flex', flexDirection: 'row' , justifyContent: 'space-between' }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, alignItems: 'center' }}>
-              <InfoOutlineIcon sx={{ height: 40, width: 40, color: 'coral' }} />
+              <AutoGraphIcon sx={{ height: 40, width: 40, color: 'coral' }} />
               <Typography variant="h5" sx={{ fontWeight: 'bold' }}>FootLocker Releases</Typography>
             </Box>
             <ActionButtons />
